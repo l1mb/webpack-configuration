95e06c781ce36f824e2ec7a5641c63475df7ef1b
Jean K <sergeev.s1012@gmail.com>
2023-02-24 10:48:25 +0300

    Add prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import App from "./app"
+import App from './app'
 import React from 'react'
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
 
-const devMode = process.env.NODE_ENV === "development"
+const devMode = process.env.NODE_ENV === 'development'
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
+const container = document.getElementById('root')
+const root = createRoot(container!)
 
-if(devMode && module && module.hot){
+if (devMode && module && module.hot) {
     module.hot.accept()
 }
 
 root.render(
     <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
+        <App />
+    </React.StrictMode>
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,102 @@
-const webpack  = require('webpack')
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MinifyCssNames = require("mini-css-class-name/css-loader");
+const webpack = require('webpack')
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MinifyCssNames = require('mini-css-class-name/css-loader')
 
-const production = process.env.NODE_ENV === "production"
+const production = process.env.NODE_ENV === 'production'
 
 module.exports = {
     entry: {
-        myAppName: path.resolve(__dirname, "./src/index.tsx")
+        myAppName: path.resolve(__dirname, './src/index.tsx'),
     },
     output: {
-        path: path.resolve(__dirname, "./dist"),
-        filename: production ? '[name].[contenthash].js': '[name].js',
+        path: path.resolve(__dirname, './dist'),
+        filename: production ? '[name].[contenthash].js' : '[name].js',
     },
     module: {
         rules: [
             {
                 test: /\.jsx$/,
                 exclude: /node_modules/,
-                use: ["babel-loader"]
+                use: ['babel-loader'],
             },
             {
                 test: /\.css$|\.scss$/,
                 use: [
                     !production
-                    ? "style-loader" // it extracts style directly into html (MiniCssExtractPlugin works incorrect with hmr and modules architecture)
-                    : MiniCssExtractPlugin.loader, // it extracts styles into file *.css
-                  {
-                    loader: "css-loader", // it interprets @import and url() like import/require() and it resolves them (you can use [import *.css] into *.js).
-                    options: {
-                      modules: {
-                        auto: /\.module\.\w+$/, // enable css-modules option for files *.module*.
-                        getLocalIdent: !production
-                          ? (() => {
-                              // it simplifies classNames fo debug purpose
-                              const getHash = MinifyCssNames();
-                              return (context, localIdentName, localName, options) =>
-                                `${localName}_${getHash(context, localIdentName, localName, options)}`;
-                            })()
-                          : MinifyCssNames(
-                              // minify classNames for prod-build
-                              { excludePattern: /[_dD]/gi } // exclude '_','d','D' because Adblock blocks '%ad%' classNames
-                            ),
-                      },
+                        ? 'style-loader' // it extracts style directly into html (MiniCssExtractPlugin works incorrect with hmr and modules architecture)
+                        : MiniCssExtractPlugin.loader, // it extracts styles into file *.css
+                    {
+                        loader: 'css-loader', // it interprets @import and url() like import/require() and it resolves them (you can use [import *.css] into *.js).
+                        options: {
+                            modules: {
+                                auto: /\.module\.\w+$/, // enable css-modules option for files *.module*.
+                                getLocalIdent: !production
+                                    ? (() => {
+                                          // it simplifies classNames fo debug purpose
+                                          const getHash = MinifyCssNames()
+                                          return (
+                                              context,
+                                              localIdentName,
+                                              localName,
+                                              options
+                                          ) =>
+                                              `${localName}_${getHash(
+                                                  context,
+                                                  localIdentName,
+                                                  localName,
+                                                  options
+                                              )}`
+                                      })()
+                                    : MinifyCssNames(
+                                          // minify classNames for prod-build
+                                          { excludePattern: /[_dD]/gi } // exclude '_','d','D' because Adblock blocks '%ad%' classNames
+                                      ),
+                            },
+                        },
                     },
-                  },
-                  "css-unicode-loader", // fixes weird issue when browser sometimes doesn't render font-icons (https://stackoverflow.com/questions/69466120/troubles-with-webpack-sass-and-fontawesome/73363510#73363510)
-                  {
-                    loader: "sass-loader", // it compiles Sass to CSS, using Node Sass by default
-                    options: {
-                      additionalData: '@import "variables";', // inject this import by default in each scss-file
-                      sassOptions: {
-                        includePaths: [path.resolve(__dirname, "src/styles")], // using pathes as root
-                      },
+                    'css-unicode-loader', // fixes weird issue when browser sometimes doesn't render font-icons (https://stackoverflow.com/questions/69466120/troubles-with-webpack-sass-and-fontawesome/73363510#73363510)
+                    {
+                        loader: 'sass-loader', // it compiles Sass to CSS, using Node Sass by default
+                        options: {
+                            additionalData: '@import "variables";', // inject this import by default in each scss-file
+                            sassOptions: {
+                                includePaths: [
+                                    path.resolve(__dirname, 'src/styles'),
+                                ], // using pathes as root
+                            },
+                        },
                     },
-                  },
-                  "postcss-loader", // it provides adding vendor prefixes to CSS rules using values from Can I Use (see postcss.config.js in the project)
+                    'postcss-loader', // it provides adding vendor prefixes to CSS rules using values from Can I Use (see postcss.config.js in the project)
                 ],
-              },
-            
+            },
+
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/,
-              },
-        ]
+            },
+        ],
     },
     resolve: {
-        extensions: ["*", ".js", ".jsx", ".scss", ".ts", ".tsx"], 
+        extensions: ['*', '.js', '.jsx', '.scss', '.ts', '.tsx'],
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title:"prikol",
-            template: "./src/index.html"
+            title: 'prikol',
+            template: './src/index.html',
         }),
         new MiniCssExtractPlugin({
-            filename: production ? '[name].[contenthash].css' : "[name].css"
-        })
+            filename: production ? '[name].[contenthash].css' : '[name].css',
+        }),
     ],
     stats: 'errors-only',
     devServer: {
         port: 3001,
-        hot: true
+        hot: true,
     },
-    mode: production? "production": "development"
+    mode: production ? 'production' : 'development',
 }
